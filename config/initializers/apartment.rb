2a474abe92974a102a9662b308785cb54cbfcdc8
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-04-29 07:19:37 +0900

    Add tenant migration target

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -4,8 +4,12 @@ Apartment.configure do |config|
     LockerRoom::Member
     LockerRoom::User
   )
+
+  # migration targets
+  config.tenant_names = lambda {
+    Account.all.map(&:schema_name)
+  }
 end
 
-Apartment::Elevators::Subdomain.excluded_subdomains = %w(
-  www
-)
+Apartment::Elevators::Subdomain.excluded_subdomains = \
+  LockerRoom::Account::EXCLUDED_SUBDOMAINS
