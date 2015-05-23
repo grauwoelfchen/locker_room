abf45968c5de37e968a9af0e758aec72af90ddf3
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2015-05-23 21:58:25 +0900

    Arrange routes definition

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ require "locker_room/constraints/subdomain_required"
 LockerRoom::Engine.routes.draw do
   constraints(LockerRoom::Constraints::SubdomainRequired) do
     scope module: "account" do
-      get    :logout, to: "sessions#destroy", as: :logout
-      delete :logout, to: "sessions#destroy", as: nil
-      get    :login,  to: "sessions#new",     as: :login
-      post   :login,  to: "sessions#create",  as: nil
-      get    :signup, to: "users#new",        as: :signup
-      post   :signup, to: "users#create",     as: nil
+      get    "/logout", to: "sessions#destroy", as: :logout
+      delete "/logout", to: "sessions#destroy", as: nil
+      get    "/login",  to: "sessions#new",     as: :login
+      post   "/login",  to: "sessions#create",  as: nil
+      get    "/signup", to: "users#new",        as: :signup
+      post   "/signup", to: "users#create",     as: nil
       root "locker#index", as: :root
     end
   end
