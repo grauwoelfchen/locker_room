8ab532aebb37bd403c8c4ea7fa9024960257b816
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-06-03 03:25:21 +0900

    Add member model overriding support
    * Create Concerns::Models::Member module
    * http://guides.rubyonrails.org/engines.html
      #overriding-models-and-controllers

--- lib/locker_room.rb
+++ lib/locker_room.rb
@@ -1,6 +1,8 @@
 require "locker_room/engine"
+
 require "locker_room/concerns/scoped_to"
 require "locker_room/concerns/models/user"
+require "locker_room/concerns/models/member"
 
 require "locker_room/testing/helpers"
 
