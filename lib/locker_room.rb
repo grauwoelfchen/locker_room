5b4ebcdda8875d86512e2120d3c733eb5682fcaa
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2015-06-03 03:54:34 +0900

    Add account model overriding support
    * Create Concerns::Models::Account module
    * http://guides.rubyonrails.org/engines.html
      #overriding-models-and-controllers

--- lib/locker_room.rb
+++ lib/locker_room.rb
@@ -1,6 +1,7 @@
 require "locker_room/engine"
 
 require "locker_room/concerns/scoped_to"
+require "locker_room/concerns/models/account"
 require "locker_room/concerns/models/user"
 require "locker_room/concerns/models/member"
 
