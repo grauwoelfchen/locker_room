48b0a33274469797929fcbf888ab598b4a00cb45
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2015-09-24 02:22:39 +0900

    Remove unnecessary namespace Concerns for models

--- lib/locker_room.rb
+++ lib/locker_room.rb
@@ -1,9 +1,9 @@
 require 'locker_room/engine'
 
 require 'locker_room/concerns/scoped_to'
-require 'locker_room/concerns/models/team'
-require 'locker_room/concerns/models/user'
-require 'locker_room/concerns/models/mateship'
+require 'locker_room/models/team'
+require 'locker_room/models/user'
+require 'locker_room/models/mateship'
 
 require 'locker_room/testing/helpers'
 
