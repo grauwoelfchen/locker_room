1b98d0787f5089378456ce6fe082149160c7f417
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-10-02 05:03:31 +0900

    Update coverage report settings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,14 @@
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
-ENV['RAILS_ENV'] = 'test'
+ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../../test/dummy/config/environment.rb',  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [
   File.expand_path('../../test/dummy/db/migrate', __FILE__),
   File.expand_path('../../db/migrate', __FILE__)
 ]
+
 require 'rails/test_help'
 
 require 'minitest/mock'
