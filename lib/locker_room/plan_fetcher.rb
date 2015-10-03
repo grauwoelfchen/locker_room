07f31cae9ba254d78f268d8aa18fc45f1ab3e7f5
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-10-04 00:29:07 +0900

    Clear the code intention by syntax

--- lib/locker_room/plan_fetcher.rb
+++ lib/locker_room/plan_fetcher.rb
@@ -4,12 +4,12 @@ module LockerRoom
       Braintree::Plan.all.each do |plan|
         stored_plan = LockerRoom::Plan.find_by(braintree_id: plan.id)
         if stored_plan
-          stored_plan.update_attributes(
+          stored_plan.update_attributes!(
             :name  => plan.name,
             :price => plan.price
           )
         else
-          LockerRoom::Plan.create(
+          LockerRoom::Plan.create!(
             :braintree_id => plan.id,
             :name         => plan.name,
             :price        => plan.price,
