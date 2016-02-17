8ab3d0004166b93d7d0198243a76aad1b0197b9d
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2016-02-17 12:51:11 +0900

    Add signin/signout navigation links

--- test/controllers/locker_room/account/users_controller_test.rb
+++ test/controllers/locker_room/account/users_controller_test.rb
@@ -6,12 +6,15 @@ module LockerRoom
       locker_room_fixtures(:teams, :users, :mateships)
 
       def test_new
-        get(:new)
-        refute(assigns(:user).persisted?)
-        assert_kind_of(LockerRoom::User, assigns(:user))
-        assert_equal(assigns(:user).mateships, [])
-        assert_template(:new)
-        assert_response(:success)
+        team = locker_room_teams(:playing_piano)
+        within_subdomain(team.subdomain) do
+          get(:new)
+          refute(assigns(:user).persisted?)
+          assert_kind_of(LockerRoom::User, assigns(:user))
+          assert_equal(assigns(:user).mateships, [])
+          assert_template(:new)
+          assert_response(:success)
+        end
       end
 
       def test_create_with_validation_error
